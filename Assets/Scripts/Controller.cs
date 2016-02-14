85880346971aa274efa5da99e444fcead67102d9
Boothand <boothand@hotmail.com>
2016-02-14 02:18:25 +0100

    General controller for any *thinking* object

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	[HideInInspector]public float horizontal;
+	[HideInInspector]public float horizontalRaw;
+	[HideInInspector]public float vertical;
+	[HideInInspector]public float mouseX;
+	[HideInInspector]public float mouseY;
+
+	[HideInInspector]public bool walk;
+	[HideInInspector]public bool attack;
+	[HideInInspector]public bool attackHold;
+	[HideInInspector]public bool parry;
+	[HideInInspector]public bool parryHold;
+	[HideInInspector]public bool jump;
+	[HideInInspector]public bool jumpHold;
+	[HideInInspector]public bool crouch;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
